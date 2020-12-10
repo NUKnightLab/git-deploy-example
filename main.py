a98e90a68ae2c717fd924172886137779ffb128f
scott2b <scottbradley.mail@gmail.com>
2020-12-10 16:56:13 -0600

    reference example var in web application output

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    example_var = os.environ.get('EXAMPLE_VAR', 'EXAMPLE_VAR not set') 
+    return 'Hello, World! {{ example_var }}'
